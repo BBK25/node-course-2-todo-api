66fa8ec7df8432e23b7e8ae14d5a2a9b4779498c
Benjamin <balogh.benjamin1991@gmail.com>
2017-09-28 10:58:58 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -31,7 +32,26 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
+});
+
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+  // res.send(req.params);
 });
 
 app.listen(3000, () => {
