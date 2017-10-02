51e5ee5972f4720d8109237f0bdfcb18493f6f17
Benjamin <balogh.benjamin1991@gmail.com>
2017-10-02 13:30:12 +0200

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -125,6 +126,37 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+// var authenticate = (req, res, next) => {
+//   var token = req.header('x-auth');
+//
+//   User.findByToken(token).then((user) => {
+//     if(!user) {
+//       return Promise.reject();
+//     }
+//
+//     req.user = user;
+//     req.token = token;
+//     next();
+//   }).catch((e) => {
+//     res.status(401).send();
+//   });
+// };
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+  // var token = req.header('x-auth');
+  //
+  // User.findByToken(token).then((user) => {
+  //   if(!user) {
+  //     return Promise.reject();
+  //   }
+  //
+  //   res.send(user);
+  // }).catch((e) => {
+  //   res.status(401).send();
+  // });
 });
 
 app.listen(port, () => {
